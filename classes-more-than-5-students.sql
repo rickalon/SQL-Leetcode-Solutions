43a1f697e8c296a19312889c240232086f79def5
Ricardo Alonso <ricardoaloof@gmail.com>
2024-09-05 16:38:47 +0200

    sol v2

--- classes-more-than-5-students.sql
+++ classes-more-than-5-students.sql
@@ -2,3 +2,9 @@
 select distinct c.class
 from Courses c
 where (select count(student) from Courses where c.class=class) >= 5
+
+-- Write your PostgreSQL query statement below
+select c.class
+from Courses c
+group by c.class
+having count(c.student) >= 5
