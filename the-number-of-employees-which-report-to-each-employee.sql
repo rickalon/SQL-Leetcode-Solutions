b77c0c7fb7afd0d343ba0eadeaed992639757f01
Ricardo Alonso <ricardoaloof@gmail.com>
2024-09-09 16:24:31 +0200

    sol

--- the-number-of-employees-which-report-to-each-employee.sql
+++ the-number-of-employees-which-report-to-each-employee.sql
@@ -1,1 +1,6 @@
-
+-- Write your PostgreSQL query statement below
+select E.employee_id, E.name, count(*) as reports_count,round(avg(C.age),0) as average_age 
+from Employees E
+inner join Employees C on E.employee_id = C.reports_to 
+group by E.employee_id, E.name
+order by E.employee_id
