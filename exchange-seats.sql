98a186276033092e5315f4f3987cfce2529603fc
Ricardo Alonso <ricardoaloof@gmail.com>
2024-09-10 17:19:43 +0200

    sol

---
+++ exchange-seats.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+select
+case
+    when id%2=1 and id = (select max(id) from seat) then id
+    when id%2=1 then id+1
+    when id%2=0 then id-1
+end as id,
+student
+from Seat
+order by id
