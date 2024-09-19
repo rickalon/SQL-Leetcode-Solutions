8951e3dc46ba84aeeb5cf675e25c6427190cd95d
Ricardo Alonso <ricardoaloof@gmail.com>
2024-09-19 17:39:41 +0200

    sol

---
+++ sales-person.cpp
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+select s.name
+from SalesPerson s
+where s.sales_id not in (
+    select sales_id
+    from Orders o
+    inner join Company c on o.com_id=c.com_id
+    where c.name='RED'
+)
